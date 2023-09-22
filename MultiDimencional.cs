632c4c095bccdd2df2ef2f962e6f2298d3d355c0
null <null>
2023-09-22 12:58:12 -0600

    Add Nuevas Cosas

--- MultiDimencional.cs
+++ MultiDimencional.cs
@@ -16,7 +16,41 @@ namespace FormMultidimencional_G2_GRAJ
 
         public static MultiDimencional Leer(string A)
         {
+            //Matriz A asignada 
+            //1,2;3,4 Matriz Ejemplo
+            string[] renglones = A.Split(';') ;
 
+            string[] columnas = renglones[0].Split(',') ;
+
+            MultiDimencional m1 = new MultiDimencional(renglones.Length, columnas.Length);
+
+            for (int i=0;i<m1.M;i++)
+            {
+                columnas = renglones[i].Split(',');
+                for (int j = 0; j < m1.N;j++)
+                {
+                    m1.A[i,j] = float.Parse(columnas[j]);
+                }
+            }
+            return m1;
         }
+
+        public override string ToString()
+        {
+            string A = "";
+
+            for(int i =0; i<M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    A+=this.A[i,j].ToString();
+                }
+                A += "\n";
+            }
+
+
+            return A;
+        }
+
     }
 }
